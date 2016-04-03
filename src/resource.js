91e285f0cc4470d8e26029f2ba71f597aa39d14e
JulissaDantes <julissadcj@gmail.com>
2016-04-03 11:24:57 -0430

    Con nombres actualizados

--- src/resource.js
+++ src/resource.js
@@ -1,13 +1,15 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     Rambo : "journey_1.png",
-    Terrorist2: "RenderPedroLOAD68.png",
-    explosion: "Atomic_Explosion.png",
-    bomba: "13052336391841183542psd-black-bomb-icon.png"
+    Terrorist2: "malvado.png",
+    explosion: "explosion.png",
+    bomba: "bomba.png",
+    madera : "madera.png"
 };
 
 var g_resources = [];
 for (var i in res) {
     g_resources.push(res[i]);
 }
+
 
